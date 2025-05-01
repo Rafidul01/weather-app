1aa0fa77f8cc14fab7a93cfd97ce655b95c22f1d
Rafidul01 <rafidulhasan555@gmail.com>
2025-05-01 22:44:44 +0600

    react router, tailwindCSS, TS setup done

---
+++ src/Router/Router.tsx
@@ -0,0 +1,17 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Root from "../Root/Root";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+       
+    },
+
+]);
+
+export default router;
